549c75850a128f52865b11aec20a5889ceb1c2de
marth <marth@LAPTOP-Q2JL6C9I>
2023-01-04 16:35:26 +0100

    Worked on the UI of the app, made two buttons navigate to other pages. MainPage almost done with Layout.

--- Arduino App 16/Arduino App 16/MainPage.xaml.cs
+++ Arduino App 16/Arduino App 16/MainPage.xaml.cs
@@ -8,11 +8,23 @@ using Xamarin.Forms;
 
 namespace Arduino_App_16
 {
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+        //once the button "Sent message" is pressed, the function will call a new communications Page"
+        async void NavigateTo_Communication(object sender, EventArgs e)
+        {
+                await Navigation.PushAsync(new CommunicationPage());
+            
+        }
+        //once the button "Settings" is pressed, the function will call a new Settings Page"
+        async void NavigateTo_Settings(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SettingsPage());
+
         }
     }
 }
